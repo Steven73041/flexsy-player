27a72b07841b648a14dc1d23e46a4e4cbe28ea7d
anastasis_mastoris <anastasis.mastoris@scorerepublic.com>
2022-03-14 19:32:42 +0200

    Fix duplicate

--- src/Traits/HasWallet.php
+++ src/Traits/HasWallet.php
@@ -8,24 +8,6 @@ use function tenancy;
 trait HasWallet
 {
     /**
-     * @return void
-     */
-    public static function bootHasWallet()
-    {
-        static::created(function ($skinUser) {
-            if (tenancy()->tenant->licensee->type == 'PLATFORM') {
-                $skinUser->wallets()->create([
-                    'currency_code' => $skinUser->mainCurrency()->shortcode,
-                    'cash'          => 0,
-                    'bonus'         => 0,
-                    'sports_bonus'  => 0,
-                    'casino_bonus'  => 0,
-                ]);
-            }
-        });
-    }
-
-    /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany|PlayerWallet[]
      */
     public function wallets()
